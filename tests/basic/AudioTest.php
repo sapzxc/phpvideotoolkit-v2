8a50be6bb8a8f8e7a0277a976c5578cc7b481274
Sap <sapzxc@gmail.com>
2015-06-15 22:42:47 +0300

    * some lost changes

---
+++ tests/basic/AudioTest.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace tests\basic;
+
+use PHPVideoToolkit\Exception;
+use tests\VideoTestAbstraction;
+
+class AudioTest extends VideoTestAbstraction
+{
+	protected $sourceFilePath = null;
+
+	/**
+	 * download sample video file
+	 * @throws \PHPVideoToolkit\Exception
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->sourceFilePath = \tests\VideoTestAbstraction::getTestFile();
+	}
+
+	public function testBasicConvert()
+	{
+		$format = new \FFMpeg\Format\Audio\Mp3();
+		$format->setAudioKiloBitrate('128k');
+	}
+
+	public function testNoRotation()
+	{
+		$dstFile = TESTS_TMP_DIR.'simple.mp4';
+		$res = $this->basicConvert($this->sourceFilePath, $dstFile);
+
+		echo "\n".__METHOD__."\tCommandString: ".$res->getCommandString()."\n";
+//		echo "Output: ".serialize($res->getOutput())."\n";
+
+		$this->assertFalse($res->hasError(), "Conversion error: ".serialize($res->getMessages()));
+
+		$this->assertFileExists($dstFile, "Dst file does not exists");
+
+		$this->assertGreaterThan(0, filesize($dstFile), "Output file empty");
+
+		// check dimensions
+		$srcVideo = new \PHPVideoToolkit\Video($this->sourceFilePath, $this->toolkitConfig);
+		$srcDimensions = $srcVideo->readDimensions(false);
+
+		$dstVideo = new \PHPVideoToolkit\Video($dstFile, $this->toolkitConfig);
+		$dstDimensions = $dstVideo->readDimensions(false);
+
+		$this->assertEquals($srcDimensions, $dstDimensions);
+	}
+
+	public function testRotation90()
+	{
+		$dstFile = TESTS_TMP_DIR.'rotated90.mp4';
+		$res = $this->basicConvert($this->sourceFilePath, $dstFile, 90);
+
+		echo "\n".__METHOD__."\tCommandString: ".$res->getCommandString()."\n";
+//		echo "Output: ".serialize($res->getOutput())."\n";
+
+		$this->assertFalse($res->hasError(), "Conversion error: ".serialize($res->getMessages()));
+
+		$this->assertFileExists($dstFile, "Dst file does not exists");
+
+		$this->assertGreaterThan(0, filesize($dstFile), "Output file empty");
+
+		// check dimensions
+		$srcVideo = new \PHPVideoToolkit\Video($this->sourceFilePath, $this->toolkitConfig);
+		$srcDimensions = $srcVideo->readDimensions(false);
+
+		$dstVideo = new \PHPVideoToolkit\Video($dstFile, $this->toolkitConfig);
+		$dstDimensions = $dstVideo->readDimensions(false);
+		//swap width and height for dst and then assert
+		$w=$dstDimensions['width'];
+		$dstDimensions['width'] = $dstDimensions['height'];
+		$dstDimensions['height'] = $w;
+
+		$this->assertEquals($srcDimensions, $dstDimensions);
+	}
+
+	public function testRotation270()
+	{
+		$dstFile = TESTS_TMP_DIR.'rotated270.mp4';
+		$res = $this->basicConvert($this->sourceFilePath, $dstFile, 90);
+
+		echo "\n".__METHOD__."\tCommandString: ".$res->getCommandString()."\n";
+//		echo "Output: ".serialize($res->getOutput())."\n";
+
+		$this->assertFalse($res->hasError(), "Conversion error: ".serialize($res->getMessages()));
+
+		$this->assertFileExists($dstFile, "Dst file does not exists");
+
+		$this->assertGreaterThan(0, filesize($dstFile), "Output file empty");
+
+		// check dimensions
+		$srcVideo = new \PHPVideoToolkit\Video($this->sourceFilePath, $this->toolkitConfig);
+		$srcDimensions = $srcVideo->readDimensions(false);
+
+		$dstVideo = new \PHPVideoToolkit\Video($dstFile, $this->toolkitConfig);
+		$dstDimensions = $dstVideo->readDimensions(false);
+		//swap width and height for dst and then assert
+		$w=$dstDimensions['width'];
+		$dstDimensions['width'] = $dstDimensions['height'];
+		$dstDimensions['height'] = $w;
+
+		$this->assertEquals($srcDimensions, $dstDimensions);
+	}
+
+	public function testRotation180()
+	{
+		$dstFile = TESTS_TMP_DIR.'rotated180.mp4';
+		$res = $this->basicConvert($this->sourceFilePath, $dstFile, 90);
+
+		echo "\n".__METHOD__."\tCommandString: ".$res->getCommandString()."\n";
+//		echo "Output: ".serialize($res->getOutput())."\n";
+
+		$this->assertFalse($res->hasError(), "Conversion error: ".serialize($res->getMessages()));
+
+		$this->assertFileExists($dstFile, "Dst file does not exists");
+
+		$this->assertGreaterThan(0, filesize($dstFile), "Output file empty");
+
+		// check dimensions
+		$srcVideo = new \PHPVideoToolkit\Video($this->sourceFilePath, $this->toolkitConfig);
+		$srcDimensions = $srcVideo->readDimensions(false);
+
+		$dstVideo = new \PHPVideoToolkit\Video($dstFile, $this->toolkitConfig);
+		$dstDimensions = $dstVideo->readDimensions(false);
+
+		$this->assertEquals($srcDimensions, $dstDimensions);
+	}
+
+	/**
+	 * @param $srcFile
+	 * @param $dstFile
+	 * @param null|int|bool $rotation
+	 * @return \PHPVideoToolkit\FfmpegProcessProgressable
+	 */
+	protected function basicConvert($srcFile, $dstFile, $rotation=null)
+	{
+		$outputFormat = new \PHPVideoToolkit\VideoFormat_Mp4('output', $this->toolkitConfig);
+		$outputFormat->addCommand('-profile:v','main');
+		$outputFormat
+			->setVideoCodec('libx264')
+			->setVideoBitrate('2500k')
+			->setVideoDimensions(\PHPVideoToolkit\VideoFormat::DIMENSION_HD720)
+
+			->setAudioCodec('aac')
+			->setAudioBitrate('96k')
+			->setAudioChannels(2)
+			->setAudioSampleFrequency(48000)
+		;
+
+		if($rotation===true || is_numeric($rotation))
+		{
+			$outputFormat->setVideoRotation($rotation);
+		}
+
+		$video = new \PHPVideoToolkit\Video($srcFile, $this->toolkitConfig);
+
+		return $video->save($dstFile, $outputFormat, \PHPVideoToolkit\Media::OVERWRITE_EXISTING);
+	}
+}
--- tests/video/VideoTest.php
+++ tests/video/VideoTest.php
@@ -16,7 +16,7 @@ class VideoTest extends VideoTestAbstraction
 
 		// biggest dimensions
 		$calcultedDimensions = $srcVideo->getOptimalDimensions(1024, 768);
-		$this->assertEquals($calcultedDimensions, array(
+		$this->assertEquals(array(
 			  "padded_width"	=> 640,
 			  "padded_height"	=> 480,
 			  "video_width"		=> 640,
@@ -25,11 +25,11 @@ class VideoTest extends VideoTestAbstraction
 			  "pad_right"		=> 0,
 			  "pad_bottom"		=> 0,
 			  "pad_left"		=> 0,
-		));
+		),$calcultedDimensions);
 
 		// biggest source video
 		$calcultedDimensions = $srcVideo->getOptimalDimensions(320, 240);
-		$this->assertEquals($calcultedDimensions, array(
+		$this->assertEquals(array(
 			"padded_width"	=> 320,
 			"padded_height"	=> 240,
 			"video_width"	=> 640,
@@ -38,11 +38,11 @@ class VideoTest extends VideoTestAbstraction
 			"pad_right"		=> 0,
 			"pad_bottom"	=> 0,
 			"pad_left"		=> 0,
-		));
+		),$calcultedDimensions);
 
-		// rotated view
+		// rotated view by 90deg
 		$calcultedDimensions = $srcVideo->getOptimalDimensions(768, 1024);
-		$this->assertEquals($calcultedDimensions, array(
+		$this->assertEquals(array(
 			"padded_width"	=> 640,
 			"padded_height"	=> 480,
 			"video_width"	=> 640,
@@ -51,7 +51,7 @@ class VideoTest extends VideoTestAbstraction
 			"pad_right"		=> 0,
 			"pad_bottom"	=> 144,
 			"pad_left"		=> 0,
-		));
+		),$calcultedDimensions);
 	}
 
 }
